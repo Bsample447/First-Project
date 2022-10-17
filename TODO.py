e98e40d57277bf448213989e832a89416584766e
Brandon S Sample <brandonsample447@gmail.com>
2022-10-17 11:52:32 -0400

    Create TODO.py

---
+++ TODO.py
@@ -0,0 +1,12 @@
+#Take input from user and assign it to variables
+
+
+print("What is your first & Last name?")
+input_name = input()
+
+print("What Task would you like to add?")
+input_task = input()
+
+print(input_name)
+print(input_task)
+
