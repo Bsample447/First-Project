5655e208acba554d64e0c971f91709b79602a157
Brandon S Sample <brandonsample447@gmail.com>
2022-10-20 08:33:34 -0400

    defining functions

--- project.py
+++ project.py
@@ -1,1 +1,27 @@
-test
+def printMenu():
+    print("1:) Add  2) List  3) Delete   4) Count Tasks  5)  Quit")
+
+
+def acceptInput(): pass
+
+
+def handleInput(userInput):
+    print("quitting")
+    return True
+
+
+
+def main():
+    _quit = False
+    
+    while not _quit:
+        printMenu()
+
+        userInput = acceptInput()
+        _quit = handleInput(userInput)
+
+
+
+if __name__ == "__main__":
+    main()
+
