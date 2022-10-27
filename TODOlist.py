a9a7431d4df6c187f0fe0842d30e79922c58e350
Brandon S Sample <brandonsample447@gmail.com>
2022-10-26 23:48:09 -0400

    Update TODOlist.py
    added strikethrough
    added never putting completed function first
    added ability to complete tasks

--- TODOlist.py
+++ TODOlist.py
@@ -1,41 +1,62 @@
 from os import system
 import os
 import sys
+import json
 
 
-allTasks = []
 
-def getFilename(user):
-    return os.path.expanduser(f"~/.ThingsToDo.txt{user}")
+currentPage = 0
+pageStep = 5
+allTasks = [[]] 
 
 
-def getTasks(user):
-    fileName = getFilename(user)
+def markCompleted(index):
+    global allTasks
+    global writeTasks
+    if not allTasks[index][0]:
+        allTasks[index][0] = True
+    else:
+        print("That task is already completed")
+
+    
+def strike(text):
+    result = ''
+    for c in text:
+        result = result + c + '\u0336'
+    return result
+
+def CompleteTask():
+    index = takeintfromuser(1,len(allTasks), "Task to mark Complete: ")
+    markCompleted(index-1)
+
+def thiswritesthefilename(user):
+    return os.path.expanduser(f"~/.ThingsToDo{user}.txt")
+
+
+def thiscallsthetasks(user):
+    fileName = thiswritesthefilename(user)
 
     if not os.path.exists(fileName): return []
 
-    toReturn = []
+    toReturn = [[]]
     
     with open(fileName) as f:
         newline = f.readline()
-
-        while newline != "":
-            toReturn += [newline.strip()]
-
-            newline = f.readline()
+        toReturn = json.loads(newline)
             
+    print(toReturn)
     return toReturn
 
 
 def writeTasks(user, tasks):
-    filename = getFilename(user)
+    filename = thiswritesthefilename(user)
     
+    jsonString = json.dumps(tasks)
     with open(filename, "w") as f:
-        for t in tasks:
-            f.write(f'{t}\n')
+        f.write(jsonString)
         
 
-def elicitInt(_min, _max, msg=None):
+def takeintfromuser(_min, _max, msg=None):
     if msg == None:
         msg = f"enter a valid interget between {_min} and {_max}: "
     
@@ -50,7 +71,7 @@ def elicitInt(_min, _max, msg=None):
             if _min <= _in <= _max:
                 valid = True 
             else:
-                print("Please keep your Input between 1-5")
+                print("Please keep your Input between 1-7")
         
         except ValueError:
             print("Please choose from one of the available options with a number!")
@@ -59,13 +80,27 @@ def elicitInt(_min, _max, msg=None):
         
 
 def printMenu():
-    ListTasks()
+    printPage()
     print("\nHello there! Above is a list of all your Task reminders currently set!")
-    print("\n1: Add a task \n2: List of current Tasks \n3: Remove a Task  \n4: see how many Tasks you have \n5: Exit Task maker")
+    print("\n1: Add a task \n2: List of current Tasks \n3: Remove a Task  \n4: see how many Tasks you have \n5: Next Page \n6: Previous Page \n7: Complete a Task \n8: Exit TODO lists ")
 
 
-def acceptMenuInput():
-    return elicitInt(1, 5, "Select a menu item: ")
+def Menuinputinteraction():
+    return takeintfromuser(1, 8, "Select a menu item: ")
+
+def CheckTasks():
+    global allTasks
+    if allTasks[0][0]:
+        index = 1
+        while True:
+            if allTasks[index][0]:
+                index += 1
+            else:
+                break
+        temp = allTasks[index]
+        allTasks[index] = allTasks[0]
+        allTasks[0] = temp
+
 
 
 def addTask():
@@ -73,23 +108,56 @@ def addTask():
 
     task = input("Enter a task to add: ")
 
-    allTasks += [task]
+    allTasks.append([False,task])
 
 def ListTasks():
-    for i in range(len(allTasks)):
+    for i in allTasks:
         print(f"{i + 1}. {allTasks[i]}")
 
 def DeleteTasks():
-    toDelete = elicitInt(1, len(allTasks), "Specify a task to delete: ")
+    toDelete = takeintfromuser(1, len(allTasks), "Specify a task to delete: ")
 
     del allTasks[toDelete - 1]
+    
+
+def printPage():
+    global allTasks
+    global currentPage
+    global pageStep
+
+    index = currentPage * pageStep
+    end = (currentPage + 1) * pageStep
+    if end > len(allTasks):
+        end = len(allTasks) 
+
+    for i in range(index,end):
+        if allTasks[i][0]:
+            print(strike(f"{i+1}) {allTasks[i][1]}"))
+        else:
+            print(f"{i+1}) {allTasks[i][1]}")
+
+def nextPage():
+  global currentPage 
+  global pageStep
+  global allTasks
+  currentPage = currentPage + 1
+  if currentPage * pageStep > len(allTasks):
+    currentPage = 0
+
+def previousPage():
+  global currentPage
+  if currentPage == 0:
+    print("Already at first page!")
+  else:
+    currentPage = currentPage - 1    
+
 
 def CountTasks():
     print(f"\nThere are {len(allTasks)} tasks left to do.")
     input("Press enter to continue...")
 
 
-def handleMenuInput(userInput):
+def whattodowithmenuinput(userInput):
     _quit = False
 
     if userInput == 1:
@@ -101,6 +169,12 @@ def handleMenuInput(userInput):
     elif userInput == 4:
         CountTasks()
     elif userInput == 5:
+         nextPage()
+    elif userInput == 6:
+        previousPage()
+    elif userInput == 7:
+        CompleteTask()
+    elif userInput == 8:
         _quit = True
     else:
         raise ValueError("Not a valid Input")
@@ -123,18 +197,21 @@ def main():
         user = f"_{sys.argv[1]}"
 
     global allTasks
-    allTasks = getTasks(user)
+    allTasks = thiscallsthetasks(user)
 
     _quit = False
     
     while not _quit:
+          CheckTasks()
+          
           ClearScreen()
+
 
           printMenu()
     
-          userInput = acceptMenuInput()
+          userInput = Menuinputinteraction()
 
-          _quit = handleMenuInput(userInput)
+          _quit = whattodowithmenuinput(userInput)
     
     writeTasks(user, allTasks)
     
